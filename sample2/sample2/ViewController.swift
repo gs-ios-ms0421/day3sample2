db512ec89669b8237083c852758836ac2a8b1d12
motonao-suzue <2017.onlineuse@gmail.com>
2020-05-30 14:50:34 +0900

    first commit.

---
+++ sample2/sample2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sample2
+//
+//  Created by 鈴江元尚 on 2020/05/30.
+//  Copyright © 2020 鈴江元尚. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
